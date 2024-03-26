ba459a2cefa51b15c8395bfefcabd18bf77723d1
weiner rogerio <weinerrogerio@gmail.com>
2024-03-26 19:47:59 -0300

    validação front-end login e cadastro

---
+++ frontend/modules/Login.js
@@ -0,0 +1,42 @@
+/* const validator = require('validator') */
+import validator from 'validator'
+
+export default class Login {
+    constructor(formClass) {
+        this.form = document.querySelector(formClass)
+    }
+    init() {
+        this.events()
+    }
+    events() {
+        if (!this.form) return
+        this.form.addEventListener('submit', e => {
+            e.preventDefault()
+            console.log('form nao enviado');
+            this.validate(e)
+        })
+    }
+    validate(e) {
+        const el = e.target;
+        const emailInput = el.querySelector('input[name="email"]')
+        const passwordInput = el.querySelector('input[name="password"]')
+        console.log(emailInput.value, passwordInput.value);
+        let error = false
+        if (!validator.isEmail(emailInput.value)) {
+            // aqui pode ter algo mais elaborado do que um alert
+            alert('Email inválido')
+            error = true
+        }
+
+
+        if (passwordInput.value.length < 3 || passwordInput.value.length > 50) {
+            alert('Senha precisa ter entre 3 e 50 caracteres')
+            error = true
+        }
+
+        if(!error) el.submit()
+        
+        
+
+    }
+}
